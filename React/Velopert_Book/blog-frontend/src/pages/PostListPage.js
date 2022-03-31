f4dc2549ae56e40aa003ea50d4e2138f0f977532
leehyeeun <leehe0430@gmail.com>
2022-03-31 17:13:11 +0900

    Button component

--- React/Velopert_Book/blog-frontend/src/pages/PostListPage.js
+++ React/Velopert_Book/blog-frontend/src/pages/PostListPage.js
@@ -1,4 +1,11 @@
+import Button from '../components/common/Button';
+
 const PostListPage = () => {
-	return <div>포스트 리스트</div>;
+	return (
+		<div>
+			포스트 리스트
+			<Button>버튼</Button>
+		</div>
+	);
 };
 export default PostListPage;
