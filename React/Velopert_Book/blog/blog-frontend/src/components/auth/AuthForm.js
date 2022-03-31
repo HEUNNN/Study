faf9fcf9b8245561c0a470e3a51e48e97259275c
leehyeeun <leehe0430@gmail.com>
2022-03-31 19:13:16 +0900

    blog file 생성

---
+++ React/Velopert_Book/blog/blog-frontend/src/components/auth/AuthForm.js
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+/**
+ * 회원가입 혹은 로그인폼을 보여준다.
+ * AuthTemplate 레이아웃 내부에 AuthForm인 로그인폼 or 회원가입폼이 들어간다.
+ */
+const AuthFormBlock = styled.div``;
+
+const AuthForm = () => {
+	return <AuthFormBlock>AuthForm</AuthFormBlock>;
+};
+export default AuthForm;
---
+++ React/Velopert_Book/blog/blog-frontend/src/pages/LoginPage.js
@@ -0,0 +1,11 @@
+import AuthForm from '../components/auth/AuthForm';
+import AuthTemplate from '../components/auth/AuthTemplate';
+
+const LoginPage = () => {
+	return (
+		<AuthTemplate>
+			<AuthForm />
+		</AuthTemplate>
+	);
+};
+export default LoginPage;
