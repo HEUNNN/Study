eca0744a9c2233634bbc6143dcbb274dd7def3a3
leehyeeun <leehe0430@gmail.com>
2022-04-01 16:01:16 +0900

    login, register redux

--- React/Velopert_Book/blog/blog-frontend/src/components/auth/AuthForm.js
+++ React/Velopert_Book/blog/blog-frontend/src/components/auth/AuthForm.js
@@ -55,7 +55,7 @@ const textMap = {
 	login: '로그인',
 	register: '회원가입',
 };
-const AuthForm = ({ type }) => {
+const AuthForm = ({ type, form, onChange, onSubmit }) => {
 	const text = textMap[type];
 	return (
 		<AuthFormBlock>
@@ -65,12 +65,16 @@ const AuthForm = ({ type }) => {
 					autoComplete="username"
 					name="username"
 					placeholder="아이디"
+					onChange={onChange}
+					value={form.username}
 				/>
 				<StyledInput
 					autoComplete="new-password"
 					name="password"
 					placeholder="비밀번호"
 					type="password"
+					onChange={onChange}
+					value={form.password}
 				/>
 				{type === 'register' && (
 					<StyledInput
@@ -78,6 +82,8 @@ const AuthForm = ({ type }) => {
 						name="passwordConfirm"
 						placeholder="비밀번호 확인"
 						type="password"
+						onChange={onChange}
+						onSubmit={onsubmit}
 					/>
 				)}
 				<ButtonWithMarginTop cyan fullwidth>
--- React/Velopert_Book/blog/blog-frontend/src/modules/auth.js
+++ React/Velopert_Book/blog/blog-frontend/src/modules/auth.js
@@ -1,17 +1,45 @@
 import { createAction, handleActions } from 'redux-actions';
+import produce from 'immer';
 
 //액션 타입 정의
-const SAMPLE_ACTION = 'auth/SAMPLE_ACTION';
+const CHANGE_FIELD = 'auth/CHANGE_FIELD';
+const INITIALIZE_FORM = 'auth/INITIALIZE_FORM';
 
 //액션 생성 함수 정의
-export const sampleAction = createAction(SAMPLE_ACTION);
+export const changeField = createAction(
+	CHANGE_FIELD,
+	({ form, key, value }) => ({
+		form,
+		key,
+		value,
+	}),
+);
+export const initializeForm = createAction(INITIALIZE_FORM, (form) => form);
 
 //리듀서 정의
 // 초기값 미리 설정해줘야 한다.
-const initialState = {};
+const initialState = {
+	register: {
+		username: '',
+		password: '',
+		passwordConfirm: '',
+	},
+	login: {
+		username: '',
+		password: '',
+	},
+};
+
 const auth = handleActions(
 	{
-		[SAMPLE_ACTION]: (state, action) => state,
+		[CHANGE_FIELD]: (state, { payload: { form, key, value } }) =>
+			produce(state, (draft) => {
+				draft[form][key] = value; // state.login.username을 변경함
+			}),
+		[INITIALIZE_FORM]: (state, { payload: form }) => ({
+			...state,
+			[form]: initialState[form],
+		}),
 	},
 	initialState,
 );
