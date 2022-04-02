f004b0a47464acc05bd063a105cc5bfd38e35f48
leehyeeun <leehe0430@gmail.com>
2022-04-02 18:07:59 +0900

    API request with redux saga

--- React/Velopert_Book/redux-middleware/src/modules/sample.js
+++ React/Velopert_Book/redux-middleware/src/modules/sample.js
@@ -1,16 +1,63 @@
+import { createAction } from "redux-actions";
 import { handleActions } from "redux-actions";
+import { call, put, take, takeLatest } from "redux-saga/effects";
+import { startLoading, finishLoading } from "./loading";
 import * as api from "../lib/api";
-import createRequestThunk from "../lib/createRequestThunk";
+
 //액션 타입 설정
 const GET_POST = "sample/GET_POST";
 const GET_POST_SUCCESS = "sample/GET_POST_SUCCESS";
+const GET_POST_FAILURE = "sample/GET_POST/FAILURE";
 
 const GET_USERS = "sample/GET_USERS";
 const GET_USERS_SUCCESS = "sample/GET_USERS_SUCCESS";
+const GET_USERS_FAILURE = "sample/GET_USERS_FAILURE";
 
-//redux-thunk는 객체 형태의 액션이 아닌, 함수 형태의 액션을 디스패치한다.
-export const getPost = createRequestThunk(GET_POST, api.getPost);
-export const getUsers = createRequestThunk(GET_USERS, api.getUsers);
+export const getPost = createAction(GET_POST, (id) => id);
+export const getUsers = createAction(GET_USERS);
+
+function* getPostSaga(action) {
+  yield put(startLoading(GET_POST)); // 로딩 시작
+  try {
+    const post = yield call(api.getPost, action.payload); // api.getPost(action.payload)를 의미한다.
+    console.log("action:", action);
+    yield put({
+      type: GET_POST_SUCCESS,
+      payload: post.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_POST_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_POST));
+}
+
+function* getUsersSaga(action) {
+  yield put(startLoading(GET_USERS)); // 로딩 시작
+  try {
+    const users = yield call(api.getUsers); // api.getPost(action.payload)를 의미한다.
+    console.log("action:", action);
+    yield put({
+      type: GET_USERS_SUCCESS,
+      payload: users.data,
+    });
+  } catch (e) {
+    yield put({
+      type: GET_USERS_FAILURE,
+      payload: e,
+      error: true,
+    });
+  }
+  yield put(finishLoading(GET_USERS));
+}
+
+export function* sampleSaga() {
+  yield takeLatest(GET_POST, getPostSaga);
+  yield takeLatest(GET_USERS, getUsersSaga);
+}
 
 const initialState = {
   post: null,
