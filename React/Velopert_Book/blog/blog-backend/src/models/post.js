20ac732070cc2eadebba82dd650c710dec1f6e44
leehyeeun <leehe0430@gmail.com>
2022-04-04 11:36:43 +0900

    blog-backend comment 수정

--- React/Velopert_Book/blog/blog-backend/src/models/post.js
+++ React/Velopert_Book/blog/blog-backend/src/models/post.js
@@ -3,6 +3,7 @@ import mongoose from 'mongoose';
 const { Schema } = mongoose;
 
 const PostSchema = new Schema({
+  // 스키마 객체 생성
   title: String,
   body: String,
   tags: [String], //문자열로 이루어진 배열
@@ -15,6 +16,6 @@ const PostSchema = new Schema({
     username: String,
   },
 });
-
-const Post = mongoose.model('Post', PostSchema);
+// 모델 생성
+const Post = mongoose.model('Post', PostSchema); // 파라미터: 스키마 이름, 스키마 객체
 export default Post;
