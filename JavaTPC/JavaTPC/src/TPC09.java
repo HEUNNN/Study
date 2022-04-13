6298f568689e0ab00d78c237548e9038f1145484
leehyeeun <leehe0430@gmail.com>
2022-04-13 10:29:32 +0900

    JVM Memory Model

--- JavaTPC/JavaTPC/src/TPC09.java
+++ JavaTPC/JavaTPC/src/TPC09.java
@@ -2,7 +2,7 @@ public class TPC09 {
     public static void main(String[] args) {
         int a=10;
         int b=20;
-        TPC09 tpc=new TPC09();
+        TPC09 tpc=new TPC09(); // heap area 영역에 클래스가 생성
         int res= tpc.add(a,b);
         System.out.println("non-static method result is "+res);
     }
