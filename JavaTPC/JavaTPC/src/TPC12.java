f19f2f85756f1bf5b262c3eac638c42c5ad244e8
leehyeeun <leehe0430@gmail.com>
2022-04-14 15:18:27 +0900

    생성자 overloading

--- JavaTPC/JavaTPC/src/TPC12.java
+++ JavaTPC/JavaTPC/src/TPC12.java
@@ -1,2 +1,14 @@
+import kr.bit.BookVO;
+
 public class TPC12 {
+    public static void main(String[] args) {
+        // 생성자 ->  객체 생성 & 초기화
+        // 생성자를 2개 이상 정의하여 구현하는 중복정의 가능 = overloading
+        BookVO b1 = new BookVO();
+        BookVO b2 = new BookVO("Hello", 10000);
+        // 생성 & 초기화
+        BookVO b3 = new BookVO("World", 20000, "Acompany", 200);
+
+        System.out.println(b3.title);
+    }
 }
--- JavaTPC/JavaTPC/src/kr/bit/BookVO.java
+++ JavaTPC/JavaTPC/src/kr/bit/BookVO.java
@@ -5,5 +5,21 @@ public class BookVO {
     public int price;
     public String company;
     public int page;
-    // 디폴트 생성자 메서드 생략되어 있음
+    // overloading: 하나의 클래스에 이름이 같은 메서드를 여러개 정의하는 것이다.
+    // 객체지향에서는 메서드 이름은 같아도 매개변수의 개수, 타입이 다르면 다른 메서드로 인정된다.
+    public BookVO(){ // 생성자를 overloading 하면 default 생성자가 자동으로 생성되지 않기에 따로 정의함
+        super();
+    }
+    public BookVO(String title, int price){
+        // title, price 멤버변수만 초기화하는 생성자
+        this.title = title;
+        this.price = price;
+    }
+    public BookVO(String title, int price, String company, int page){
+        // title, price, company, page 모두 초기화 하는 생성자
+        this.title = title;
+        this.price = price;
+        this.company = company;
+        this.page = page;
+    }
 }
