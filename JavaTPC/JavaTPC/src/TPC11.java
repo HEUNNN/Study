7b4415fe03252d0e1bf3412fc1717ac62341d36b
leehyeeun <leehe0430@gmail.com>
2022-04-14 14:17:52 +0900

    사용자 정의자료형 UDDT

---
+++ JavaTPC/JavaTPC/src/TPC11.java
@@ -0,0 +1,17 @@
+import kr.bit.BookVO;
+
+public class TPC11 {
+    public static void main(String[] args) {
+        // 책 1권을 저장하기위해 객체를 생성한다.
+        BookVO b =new BookVO();
+        b.title="Python";
+        b.price=12000;
+        b.company="한빛";
+        b.page=200;
+        System.out.print(b.title+"\t");
+        System.out.print(b.price+"\t");
+        System.out.print(b.company+"\t");
+        System.out.print(b.page+"\t");
+
+    }
+}
---
+++ JavaTPC/JavaTPC/src/kr/bit/BookVO.java
@@ -0,0 +1,9 @@
+package kr.bit;
+// 책(Object)를 class로 설계 -> 제목, 가격, 출판사, 페이지 수
+public class BookVO {
+    public String title;
+    public int price;
+    public String company;
+    public int page;
+    // 디폴트 생성자 메서드 생략되어 있음
+}
