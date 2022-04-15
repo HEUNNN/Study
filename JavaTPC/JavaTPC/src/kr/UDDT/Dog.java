67ac2efb668508056e9738c5a305bcada1eb1ad5
leehyeeun <leehe0430@gmail.com>
2022-04-15 17:31:22 +0900

    상속

--- JavaTPC/JavaTPC/src/kr/UDDT/Dog.java
+++ JavaTPC/JavaTPC/src/kr/UDDT/Dog.java
@@ -3,4 +3,5 @@ package kr.UDDT;
 public class Dog extends Animal{
     // 이름, 나이, 종: 객체의 상태 정보
     // 상속은 행위 정보 측면에서 바라봐야한다.
+    // Dog는 Animal 클래스를 상속받기 때문에 Animal에서 정의한 메서드를 본인의 것처럼 사용할 수 있다.
 }
