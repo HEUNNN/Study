9a09ad11685305836f6b11072b1660a6c700f869
leehyeeun <leehe0430@gmail.com>
2022-04-17 17:46:49 +0900

    Interface 활용 실습

--- JavaTPC/JavaTPC/src/kr/poly/TV.java
+++ JavaTPC/JavaTPC/src/kr/poly/TV.java
@@ -1,15 +1,30 @@
 package kr.poly;
 
 public class TV implements RemoCon {
+    int currCH = 70;
     // Interface인 TV 클래스의 abstract method를 override ->  빠짐없이 모두 override 해야한다.
     @Override
     public void chUp() {
-        System.out.println("TV CH UP");
+        if(currCH < RemoCon.MAXCH){
+            currCH ++;
+            System.out.println("TV CH UP, current CH: " + currCH);
+        }else {
+            currCH = 1;
+            System.out.println("TV CH UP, current CH: " + currCH);
+        }
+
     }
 
     @Override
     public void chDown() {
-        System.out.println("TV CH DOWN");
+        if(currCH > RemoCon.MINCH){
+            currCH --;
+            System.out.println("TV CH DOWN, current CH: " + currCH);
+        }else {
+            currCH = 100;
+            System.out.println("TV CH DOWN, current CH: " + currCH);
+        }
+
     }
 
     @Override
