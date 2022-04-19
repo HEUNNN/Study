b18922d97b2476de5aed6dd36124c26537c91722
leehyeeun <leehe0430@gmail.com>
2022-04-19 15:35:36 +0900

    정적 컨텐츠 - 스프링 웹 개발 기초

--- Spring_intro/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Spring_intro/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -3,6 +3,7 @@ package hello.hellospring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -11,5 +12,11 @@ public class HelloController {
         // Model은 MVC의 Model을 의미한다.
         model.addAttribute("data", "Spring!!");
         return "hello";
+    }
+
+    @GetMapping("hello-mvc") // 외부에서 이름을 받아온다. hello는 내부에서 직접 data를 넣어준다.
+    public String helloMVC(@RequestParam(value = "name", required = false) String name, Model model){
+        model.addAttribute("name", name);
+        return "hello-template";
     }
 }
