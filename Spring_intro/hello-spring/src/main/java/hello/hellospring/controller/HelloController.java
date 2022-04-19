c06101ba1f672d5b1309beb333864ce6829d5504
leehyeeun <leehe0430@gmail.com>
2022-04-19 14:16:21 +0900

    View 환경설정

---
+++ Spring_intro/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -0,0 +1,15 @@
+package hello.hellospring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+    @GetMapping("hello") // Web에서 '/hello'가 들어오면 hello 메서드를 호출한다.
+    public String hello(Model model) { // Spring이 모델을 만들어서 넘겨준다.
+        // Model은 MVC의 Model을 의미한다.
+        model.addAttribute("data", "Spring!!");
+        return "hello";
+    }
+}
