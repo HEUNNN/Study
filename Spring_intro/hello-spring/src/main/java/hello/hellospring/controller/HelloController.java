f7f89aee74dd6f730e60bc514b33d878b2c2dcd1
leehyeeun <leehe0430@gmail.com>
2022-04-19 16:23:33 +0900

    API - 스프링 웹 개발 기초

--- Spring_intro/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
+++ Spring_intro/hello-spring/src/main/java/hello/hellospring/controller/HelloController.java
@@ -4,6 +4,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 public class HelloController {
@@ -13,10 +14,35 @@ public class HelloController {
         model.addAttribute("data", "Spring!!");
         return "hello";
     }
-
+    // MVC
     @GetMapping("hello-mvc") // 외부에서 이름을 받아온다. hello는 내부에서 직접 data를 넣어준다.
     public String helloMVC(@RequestParam(value = "name", required = false) String name, Model model){
         model.addAttribute("name", name);
         return "hello-template";
+    }
+    // API -> Model 사용 X, View가 없고 반환되는 문자(data)가 그대로 웹에 내려간다.(소스코드를 보면 MVC와 차이를 확인해볼 수 있다.)
+    @GetMapping("hello-string")
+    @ResponseBody // HTTP의 body에 return 한 데이터를 직접 넣겠다는 의미
+    public String helloString(@RequestParam("name") String name){
+        return "hello" + name;
+    }
+
+    @GetMapping("hello-api") // JSON 방식이다.
+    @ResponseBody
+    public Hello helloApi(@RequestParam("name") String name){ // Type이 Hello인 값을 반환하는 메서드
+        Hello hello = new Hello(); // 객체를 생성(new)해야 그 자신(this)의 name에 param으로 넘겨받은 name을 set할 수 있다.
+        hello.setName(name);
+        return hello;
+    }
+    static class Hello { // static class는 같은 클래스 안에서 사용할 수 있다.
+        private String name;
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
     }
 }
