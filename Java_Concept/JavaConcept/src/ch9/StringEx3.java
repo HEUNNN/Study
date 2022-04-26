64444b40b583a336ba63bf267aa26abcec89ee73
leehyeeun <leehe0430@gmail.com>
2022-04-26 22:46:35 +0900

    문자열과 기본형간의 변환

---
+++ Java_Concept/JavaConcept/src/ch9/StringEx3.java
@@ -0,0 +1,11 @@
+package ch9;
+
+public class StringEx3 {
+    public static void main(String[] args) {
+        int iVal = 100;
+        String strVal = String.valueOf(iVal); // int -> String
+
+        double dVal = 200.0;
+        String strVal2 = String.valueOf(dVal); // double -> String
+    }
+}
