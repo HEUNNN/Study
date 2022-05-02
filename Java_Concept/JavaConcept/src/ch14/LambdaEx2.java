e2c3c3ed55c04386b1fec23051585af6392447a4
leehyeeun <leehe0430@gmail.com>
2022-05-02 17:19:45 +0900

    Lambda 형변환

--- Java_Concept/JavaConcept/src/ch14/LambdaEx2.java
+++ Java_Concept/JavaConcept/src/ch14/LambdaEx2.java
@@ -1,14 +1,14 @@
 package ch14;
 
 @FunctionalInterface
-interface Myfunction {
+interface Myfunction2 {
     void myMethod(); // public abstract void myMethod();
 }
 public class LambdaEx2 {
     public static void main(String[] args) {
-        MyFunction f = () -> {}; // (MyFunction) 형변환이 생략됨
-        Object obj = (MyFunction)() -> {}; // (Object) 형변환이 생략됨
-        String str = ((Object)(MyFunction)() -> {}).toString();
+        Myfunction2 f = () -> {}; // (MyFunction2) 형변환이 생략됨
+        Object obj = (Myfunction2)() -> {}; // (Object) 형변환이 생략됨
+        String str = ((Object)(Myfunction2)() -> {}).toString();
 
         System.out.println(f);
         System.out.println(obj);
