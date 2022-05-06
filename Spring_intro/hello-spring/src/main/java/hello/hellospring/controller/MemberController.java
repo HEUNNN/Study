92b59a18ea161e096f3ab862fc66866f259f7c18
leehyeeun <leehe0430@gmail.com>
2022-05-06 22:50:57 +0900

    회원 웹 기능 - 조회

--- Spring_intro/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
+++ Spring_intro/hello-spring/src/main/java/hello/hellospring/controller/MemberController.java
@@ -4,8 +4,11 @@ import hello.hellospring.domain.Member;
 import hello.hellospring.service.MemberService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+
+import java.util.*;
 
 @Controller
 public class MemberController {
@@ -26,9 +29,18 @@ public class MemberController {
     public String create(MemberForm form) {
         Member member = new Member();
         member.setName(form.getName());
-
+        System.out.println(form.getName());
+        System.out.println(member.getName());
         memberService.join(member);
 
         return "redirect:/"; // 회원가입이 끝나면 '/'으로 보
+    }
+
+    @GetMapping("/members")
+    public String list(Model model) {
+        List<Member> members = memberService.findMembers();
+
+        model.addAttribute("members", members); // key: "members", value: members (List<Member> type)
+        return "members/memberList";
     }
 }
