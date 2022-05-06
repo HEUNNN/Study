c31ef52b725a67a0790359d8e239d2a2305a38e9
leehyeeun <leehe0430@gmail.com>
2022-05-06 16:52:17 +0900

    컴포넌트 스캔과 자동의존관계 설정

--- Spring_intro/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
+++ Spring_intro/hello-spring/src/main/java/hello/hellospring/service/MemberService.java
@@ -3,15 +3,19 @@ package hello.hellospring.service;
 import hello.hellospring.domain.Member;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class MemberService {
     // 회원 서비스를 만들기 위해서는 member repository가 필요
     private final MemberRepository memberRepository;
 
-    public MemberService(MemberRepository memberRepository) {
+    @Autowired
+    public MemberService(MemberRepository memberRepository) { // 생성자
         this.memberRepository = memberRepository;
     }
 // MemberRepository 는 Interface이고 이 Interface를 implement(구현)한 것이 MemoryMemberRepository이다.
