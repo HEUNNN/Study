8939efbb489b7b463861dca581b856226239b6a4
leehyeeun <leehe0430@gmail.com>
2022-05-08 14:12:45 +0900

    순수 JDBC

--- Spring_intro/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
+++ Spring_intro/hello-spring/src/main/java/hello/hellospring/SpringConfig.java
@@ -1,13 +1,25 @@
 package hello.hellospring;
 
+import hello.hellospring.repository.JdbcMemoryRepository;
 import hello.hellospring.repository.MemberRepository;
 import hello.hellospring.repository.MemoryMemberRepository;
 import hello.hellospring.service.MemberService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
+import javax.sql.DataSource;
+
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
     @Bean // Spring Bean 등록 -  코드로 직접하기
     public MemberService memberService() {
         return new MemberService(memberRepository());
@@ -15,6 +27,8 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+
+//        return new MemoryMemberRepository();
+        return new JdbcMemoryRepository(dataSource);
     }
 }
