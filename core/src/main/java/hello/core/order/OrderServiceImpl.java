d704a63d777bd49cdb3862bf84abb83535da56e3
leehyeeun <leehe0430@gmail.com>
2022-05-09 21:18:12 +0900

    Order and discount domain dev

---
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -0,0 +1,26 @@
+package hello.core.order;
+
+import hello.core.discount.DiscountPolicy;
+import hello.core.discount.FixDiscountPolicy;
+import hello.core.member.Member;
+import hello.core.member.MemberRepository;
+import hello.core.member.MemoryMemberRepository;
+
+public class OrderServiceImpl implements OrderService{
+    // 주문 서비스 구현체의 역할
+
+    // 1. 회원 저장소 구현체에 회원을 조회
+    MemberRepository memberRepository = new MemoryMemberRepository();
+    // 2. 할인 정책 구현체에 할인적용을 조회
+    DiscountPolicy discountPolicy = new FixDiscountPolicy();
+
+    @Override
+    public Order createOrder(Long memberId, String itemName, int itemPrice) {
+        Member member = memberRepository.findById(memberId);
+        int discountPrice = discountPolicy.discount(member, itemPrice);
+        return new Order(memberId, itemName,itemPrice, discountPrice);
+        // 단일 체계 설계 규칙이 잘 지켜짐
+    }
+
+    // 주문 생성 서비스가 주문(Order)을 생성하여 반환한다.
+}
