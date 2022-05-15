2aba298f4b723aa860f3c75cf369ebcc6c408f88
leehyeeun <leehe0430@gmail.com>
2022-05-15 22:08:17 +0900

    order entity 수정

--- core/src/main/java/hello/core/order/Order.java
+++ core/src/main/java/hello/core/order/Order.java
@@ -22,32 +22,16 @@ public class Order { // 주문서(주문 엔티티) -> Member entity 객체와 �
         return memberId;
     }
 
-    public void setMemberId(Long memberId) {
-        this.memberId = memberId;
-    }
-
     public String getItemName() {
         return itemName;
-    }
-
-    public void setItemName(String itemName) {
-        this.itemName = itemName;
     }
 
     public int getItemPrice() {
         return itemPrice;
     }
 
-    public void setItemPrice(int itemPrice) {
-        this.itemPrice = itemPrice;
-    }
-
     public int getDiscountPrice() {
         return discountPrice;
-    }
-
-    public void setDiscountPrice(int discountPrice) {
-        this.discountPrice = discountPrice;
     }
 
     @Override
