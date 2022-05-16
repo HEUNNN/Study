7ccfa1d3150928ac15540b89195ef440e09c5b1a
leehyeeun <leehe0430@gmail.com>
2022-05-16 22:36:39 +0900

    @Autowired가 DI를 위해 조회하는 type을 가진 빈이 2개 이상일  때 해결방법 1 - @Autowired + 필드명

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -4,26 +4,28 @@ import hello.core.discount.DiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-@RequiredArgsConstructor
-// Lombok의 annotaion으로 final(필수값을 의미)가 붙은 값들을 갖고 OrderServiceImpl(MemberRepositoru memberRepository, DiscountPolicy discountPolicy)와 같은 생성자를 자동으로 생성해준다.
+//@RequiredArgsConstructor
+// Lombok의 annotaion으로 final(필수값을 의미)가 붙은 값들을 갖고 OrderServiceImpl(MemberRepositoru memberRepository, DiscountPolicy discountPolicy)와 같은 생성자를 자동으로 생성해준다.(생성자 필요없어짐)
 public class OrderServiceImpl implements OrderService{
 
     private final MemberRepository memberRepository;
-    private final DiscountPolicy discountPolicy;
+    private final DiscountPolicy rateDiscountPolicy;
 
     // 생성자가 1개면 @Autowired 생략 가능
-//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // 생성자 주입
-//        this.memberRepository = memberRepository;
-//        this.discountPolicy = discountPolicy;
-//    }
+    @Autowired
+    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy rateDiscountPolicy) { // 생성자 주입
+        this.memberRepository = memberRepository;
+        this.rateDiscountPolicy = rateDiscountPolicy;
+    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
         Member member = memberRepository.findById(memberId);
-        int discountPrice = discountPolicy.discount(member, itemPrice);
+        int discountPrice = rateDiscountPolicy.discount(member, itemPrice);
         return new Order(memberId, itemName,itemPrice, discountPrice);
         // 단일 체계 설계 규칙이 잘 지켜짐
     }
