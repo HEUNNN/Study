1902ff81719622322ad879f98b29a25b72c00f64
leehyeeun <leehe0430@gmail.com>
2022-05-16 22:15:25 +0900

    생성자가 하나일 때 @Autowired를 생략하고 Lombok의 @RequiredArgsConstructor를 사용하여 생성자를 자동으로 생성

--- core/src/main/java/hello/core/order/OrderServiceImpl.java
+++ core/src/main/java/hello/core/order/OrderServiceImpl.java
@@ -1,29 +1,24 @@
 package hello.core.order;
 
 import hello.core.discount.DiscountPolicy;
-import hello.core.discount.FixDiscountPolicy;
-import hello.core.discount.RateDiscountPolicy;
 import hello.core.member.Member;
 import hello.core.member.MemberRepository;
-import hello.core.member.MemoryMemberRepository;
-import org.springframework.beans.factory.annotation.Autowired;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
+@RequiredArgsConstructor
+// Lombok의 annotaion으로 final(필수값을 의미)가 붙은 값들을 갖고 OrderServiceImpl(MemberRepositoru memberRepository, DiscountPolicy discountPolicy)와 같은 생성자를 자동으로 생성해준다.
 public class OrderServiceImpl implements OrderService{
-    // 주문 서비스 구현체의 역할
 
-    // 1. 회원 저장소 구현체에 회원을 조회
     private final MemberRepository memberRepository;
-    // 2. 할인 정책 구현체에 할인적용을 조회
-//    private final DiscountPolicy discountPolicy = new FixDiscountPolicy();
     private final DiscountPolicy discountPolicy;
 
-    @Autowired
-    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // 생성자 주입
-        this.memberRepository = memberRepository;
-        this.discountPolicy = discountPolicy;
-    }
+    // 생성자가 1개면 @Autowired 생략 가능
+//    public OrderServiceImpl(MemberRepository memberRepository, DiscountPolicy discountPolicy) { // 생성자 주입
+//        this.memberRepository = memberRepository;
+//        this.discountPolicy = discountPolicy;
+//    }
 
     @Override
     public Order createOrder(Long memberId, String itemName, int itemPrice) {
