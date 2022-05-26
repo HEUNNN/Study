b06d7ea856b95d4ddb0661d493bf435ec3070be7
leehyeeun <leehe0430@gmail.com>
2022-05-26 16:06:27 +0900

    다양한 컨트롤러를 사용하기 위해 adapter를 도입한 유연한 컨트롤러 2

---
+++ servlet/src/main/java/hello/servlet/web/frontcontroller/v5/adapter/ControllerV4HandlerAdapter.java
@@ -0,0 +1,43 @@
+package hello.servlet.web.frontcontroller.v5.adapter;
+
+import hello.servlet.web.frontcontroller.ModelView;
+import hello.servlet.web.frontcontroller.v4.ControllerV4;
+import hello.servlet.web.frontcontroller.v5.MyHandlerAdapter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.Map;
+
+public class ControllerV4HandlerAdapter implements MyHandlerAdapter {
+
+    @Override
+    public boolean support(Object handler) {
+        return (handler instanceof ControllerV4);
+    }
+
+    @Override
+    public ModelView handle(HttpServletRequest request, HttpServletResponse response, Object handler) throws ServletException, IOException {
+        ControllerV4 controller = (ControllerV4) handler;
+
+        Map<String, String> paramMap = createParamMap(request);
+        Map<String, Object> model = new HashMap<>();
+
+        String viewName = ((ControllerV4) handler).process(paramMap, model);
+
+        // ModelView의 viewName 필드는 private이지만 생성자에 선언이 되어있어서 setter 사용안해도 되지만, model은 private이고 생성자에 선언이 안되어 있어서 setter를 사용해야한다.
+        ModelView modelView = new ModelView(viewName);
+        modelView.setModel(model); // ModelView에 model도 세팅해줘야한다.
+
+        return modelView;
+    }
+
+    private Map<String, String> createParamMap(HttpServletRequest request) {
+        Map<String, String> paramMap = new HashMap<>();
+        request.getParameterNames().asIterator()
+                .forEachRemaining(paramName -> paramMap.put(paramName, request.getParameter(paramName)));
+        return paramMap;
+    }
+}
