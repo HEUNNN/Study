8b263e7f0d00b31734f8823b2fc1c4a957ee4809
leehyeeun <leehe0430@gmail.com>
2022-05-27 13:48:50 +0900

    spring mvc - handlerMapping & handlerAdapter

---
+++ servlet/src/main/java/hello/servlet/web/springmvc/oldversion/MyHttpRequestHandler.java
@@ -0,0 +1,17 @@
+package hello.servlet.web.springmvc.oldversion;
+
+import org.springframework.stereotype.Component;
+import org.springframework.web.HttpRequestHandler;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@Component("/springmvc/request-handler")
+public class MyHttpRequestHandler implements HttpRequestHandler { // handler와 controller는 유사한 역할을 의미한다.
+    @Override
+    public void handleRequest(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        System.out.println("MyHttpRequestHandler.handleRequest");
+    }
+}
