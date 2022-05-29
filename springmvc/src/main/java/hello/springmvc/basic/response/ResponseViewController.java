69e925c879acf945619d9216355477b81675aa93
leehyeeun <leehe0430@gmail.com>
2022-05-29 16:40:00 +0900

    response - 정적 리소스, 뷰 템플릿

---
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -0,0 +1,28 @@
+package hello.springmvc.basic.response;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class ResponseViewController {
+    @RequestMapping("/response-view-v1")
+    public ModelAndView responseViewV1() {
+        ModelAndView mav = new ModelAndView("response/hello") 
+                .addObject("data", "hello!");
+
+        return mav;
+    }
+
+    @RequestMapping("/response-view-v2")
+    public String responseViewV2(Model model) {
+        model.addAttribute("data", "hello spring!");
+        return "response/hello";
+    }
+
+    @RequestMapping("/response/hello") // @RequestMapping의 경로와 view의 논리 경로가 같으면 void로 해도 되지만 권장하는 방법은 아니다.
+    public void responseViewV3(Model model) {
+        model.addAttribute("data", "helloooo spring");
+    }
+}
