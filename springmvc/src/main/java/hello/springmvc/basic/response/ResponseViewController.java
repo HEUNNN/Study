80f89e1c9c524e1f453fa5ccbf254e17122235e2
leehyeeun <leehe0430@gmail.com>
2022-05-29 18:22:44 +0900

    HTTP 응답 - HTTP, API, 메시지 바디에 직접 입력

--- springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
+++ springmvc/src/main/java/hello/springmvc/basic/response/ResponseViewController.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.ModelAndView;
 public class ResponseViewController {
     @RequestMapping("/response-view-v1")
     public ModelAndView responseViewV1() {
-        ModelAndView mav = new ModelAndView("response/hello") 
+        ModelAndView mav = new ModelAndView("response/hello")
                 .addObject("data", "hello!");
 
         return mav;
