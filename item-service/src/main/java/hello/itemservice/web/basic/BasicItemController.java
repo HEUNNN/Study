cc26dc887e59ed39998e9b30313d8f4fc73a879f
leehyeeun <leehe0430@gmail.com>
2022-05-30 21:17:19 +0900

    상품 상세

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -6,6 +6,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import javax.annotation.PostConstruct;
@@ -25,11 +26,20 @@ public class BasicItemController {
         // BasicItemController가 스프링 컨테이너에 등록되면서 생성자 주입으로 ItemRepository가 주입된다. (ItemRepository는 @Repository가 있어서, 이미 스프링 빈에 등록되어있다.)
     }
 
+    // 상품 목록 Controller
     @GetMapping // /basic/items로 들어오는 GET request -> item 목록을 출력
     public String items(Model model) {
         List<Item> items = itemRepository.findAll();
         model.addAttribute("items", items); // model에 items라는 이름으로 List<Item> items를 저장한다. view에서는 items 이름으로 데이터를 꺼내서 사용한다.
         return "basic/items"; // basic/items에 해당하는 뷰를 반환한다는 의미이다.
+    }
+
+    // 상품 상세 Controller
+    @GetMapping("/{itemId}")
+    public String item(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/item";
     }
 
     // 테스트용 데이터 추가
