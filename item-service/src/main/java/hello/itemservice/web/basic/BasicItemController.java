6b09d1bc65aaba2dac9ad743af1d473c09a1e617
leehyeeun <leehe0430@gmail.com>
2022-05-30 23:11:51 +0900

    상품 수정 controller

--- item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
+++ item-service/src/main/java/hello/itemservice/web/basic/BasicItemController.java
@@ -33,7 +33,7 @@ public class BasicItemController {
     }
 
     // 상품 상세 Controller
-    @GetMapping("/{itemId}")
+    @GetMapping("/{itemId}") // GET /basic/items/{itemId}
     public String item(@PathVariable Long itemId, Model model) {
         Item item = itemRepository.findById(itemId);
         model.addAttribute("item", item);
@@ -72,9 +72,25 @@ public class BasicItemController {
 
     // 상품 등록 처리 V3 Controller -> @ModelAttribute의 이름 생략
     @PostMapping("/add")
-    public String addItemV3(@ModelAttribute Item item) { // @ModelAttribute의 이름을 생략하면 모델에 저장될 때 클래스 명(앞글자를 소문자로 바꾼)을 사용한다. 
+    public String addItemV3(@ModelAttribute Item item) { // @ModelAttribute의 이름을 생략하면 모델에 저장될 때 클래스 명(앞글자를 소문자로 바꾼)을 사용한다.
         itemRepository.save(item);
         return "basic/item";
+    }
+
+    // 상품 수정 폼 Controller
+    @GetMapping("/{itemId}/edit")
+    public String editForm(@PathVariable Long itemId, Model model) {
+        Item item = itemRepository.findById(itemId);
+        model.addAttribute("item", item);
+        return "basic/editForm";
+    }
+
+    // 상품 수정 처리 Controller
+    @PostMapping("/{itemId}/edit") // POST /{itemId}/edit URL이 들어오면 해당 method가 실행된다.
+    public String edit(@PathVariable Long itemId, @ModelAttribute Item item) { // 수정된 내용이 적용된 item이다.
+        itemRepository.update(itemId, item);
+        return "redirect:/basic/items/{itemId}"; // Controller mapping을 위한 url이다.
+        // return "basic/item"; 으로 하면 수정하고 나서도 url이 ~/~~/edit 이런식으로 되어 있다. -> 이건 그냥 View 이름을 반환
     }
 
     // 테스트용 데이터 추가
