a20da5caf931f5b890b553a28f68fdcc7eb12a1f
leehyeeun <leehe0430@gmail.com>
2022-06-03 16:56:00 +0900

    체크박스 - 단일1

--- Spring_MVC/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ Spring_MVC/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -3,6 +3,7 @@ package hello.itemservice.web.form;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
@@ -13,6 +14,7 @@ import java.util.List;
 @Controller
 @RequestMapping("/form/items")
 @RequiredArgsConstructor // final 혹은 @NotNull 애노테이션이 붙은 필드의 생성자를 자동으로 생성해준다.
+@Slf4j
 public class FormItemController {
 
     private final ItemRepository itemRepository;
@@ -42,7 +44,7 @@ public class FormItemController {
 
         // @ModelAttribute: POST 요청에 담겨온 파라미터 값을 특정 자바 객체에 매핑한다. 또 model 객체에 자동으로 addAttribute(item) 해준다.
         // HTML Form에서 POST 방식의 파라미터 형식 데이터를 넘겨준다.
-
+        log.info("item.getOpen={}", item.getOpen());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
