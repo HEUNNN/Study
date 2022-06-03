0befd8a693b5dfeecfec403bfc8485908a64195f
leehyeeun <leehe0430@gmail.com>
2022-06-03 21:51:23 +0900

    셀렉트 박

--- Spring_MVC/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ Spring_MVC/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -1,5 +1,6 @@
 package hello.itemservice.web.form;
 
+import hello.itemservice.domain.item.DeliveryCode;
 import hello.itemservice.domain.item.Item;
 import hello.itemservice.domain.item.ItemRepository;
 import hello.itemservice.domain.item.ItemType;
@@ -10,10 +11,7 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
-import java.util.LinkedHashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 @Controller
 @RequestMapping("/form/items")
@@ -53,6 +51,16 @@ public class FormItemController {
         return ItemType.values(); // ENUM 의 모든 정보를 배열로 반환한다.
     }
 
+    // @ModelAttribute로 deliberyCodes 셀렉트 박스 만들기
+    @ModelAttribute("deliveryCodes")
+    public List<DeliveryCode> deliveryCodes() {
+        List<DeliveryCode> deliveryCodes = new ArrayList<>();
+        deliveryCodes.add(new DeliveryCode("FAST", "빠른 배송"));
+        deliveryCodes.add(new DeliveryCode("NORMAL", "일반 배송"));
+        deliveryCodes.add(new DeliveryCode("SLOW", "느린 배송"));
+
+        return deliveryCodes;
+    }
 
     @GetMapping("/add")
     public String addForm(Model model) {
