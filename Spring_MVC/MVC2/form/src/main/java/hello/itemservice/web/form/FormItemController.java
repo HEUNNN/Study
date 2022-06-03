bdad2d8bc658befc3bd8c389d8f74c9e1b93ccb5
leehyeeun <leehe0430@gmail.com>
2022-06-03 18:33:39 +0900

    체크 박스 - 멀티

--- Spring_MVC/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
+++ Spring_MVC/MVC2/form/src/main/java/hello/itemservice/web/form/FormItemController.java
@@ -9,7 +9,10 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 @Controller
 @RequestMapping("/form/items")
@@ -33,9 +36,29 @@ public class FormItemController {
         return "form/item";
     }
 
+    // @ModelAttribute로 등록지역 체크박스를 위한 지역 데이터 넘겨주기, 이전에 사용하던 ModelAttribute와 기능이 다르다.
+    @ModelAttribute("regions")
+    public Map<String, String> regions() {
+        Map<String, String> regions = new LinkedHashMap<>(); // 순서 보장을 위해 LinkedHashMap 사용
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        return regions;
+    }
+
+
     @GetMapping("/add")
     public String addForm(Model model) {
+        // 입력 폼 처리를 위해 빈 객체 넘겨준다.
         model.addAttribute("item", new Item());
+
+        // 등록지역 체크 박스를 위해 Map data를 넘겨준다.
+/*        Map<String, String> regions = new LinkedHashMap<>();
+        regions.put("SEOUL", "서울");
+        regions.put("BUSAN", "부산");
+        regions.put("JEJU", "제주");
+        model.addAttribute("regions", regions);
+*/
         return "form/addForm";
     }
 
@@ -45,6 +68,7 @@ public class FormItemController {
         // @ModelAttribute: POST 요청에 담겨온 파라미터 값을 특정 자바 객체에 매핑한다. 또 model 객체에 자동으로 addAttribute(item) 해준다.
         // HTML Form에서 POST 방식의 파라미터 형식 데이터를 넘겨준다.
         log.info("item.getOpen={}", item.getOpen());
+        log.info("item.getRegions={}", item.getRegions());
         Item savedItem = itemRepository.save(item);
         redirectAttributes.addAttribute("itemId", savedItem.getId());
         redirectAttributes.addAttribute("status", true);
