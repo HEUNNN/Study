8e3e5845d806e8c0f3285449930b2097d100cd23
leehyeeun <leehe0430@gmail.com>
2022-06-07 22:56:57 +0900

    ch3 Q6 연속된 자연수의 합 구하기 - 투 포인터

---
+++ Algorithm/Algorithm_doit/src/main/java/Ch3_Basic/Q6TwoPointerAndSort.java
@@ -0,0 +1,36 @@
+package Ch3_Basic;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Q6TwoPointerAndSort {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    public static void main(String[] args) throws IOException {
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        int result = 1;
+
+        int start = 1;
+        int end = 1;
+        int sum = 1;
+        while (end != N) {
+            if (sum < N) {
+                end++;
+                sum += end;
+            } else if (sum == N) {
+                sum -= start;
+                start++;
+                result++;
+            } else { // sum > N
+                sum -= start;
+                start++;
+            }
+        }
+
+        System.out.println(result);
+    }
+}
