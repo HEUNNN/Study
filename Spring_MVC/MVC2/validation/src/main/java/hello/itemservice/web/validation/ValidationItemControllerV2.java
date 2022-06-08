bf95bca91707b0661f6f4f0f97591ca1f39a5989
leehyeeun <leehe0430@gmail.com>
2022-06-08 15:39:29 +0900

    오류 코드와 메시지 처리 2

--- Spring_MVC/MVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
+++ Spring_MVC/MVC2/validation/src/main/java/hello/itemservice/web/validation/ValidationItemControllerV2.java
@@ -13,9 +13,7 @@ import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
 @Slf4j
 @Controller
@@ -125,7 +123,7 @@ public class ValidationItemControllerV2 {
         return "redirect:/validation/v2/items/{itemId}";
     }
 
-    @PostMapping("/add") // new Field 생성자의 codes, args 파라미터를 사용해서 error 메시지 효율적으로 관리하기
+    // @PostMapping("/add") // new Field 생성자의 codes, args 파라미터를 사용해서 error 메시지 효율적으로 관리하기
     public String addItemV3(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
 
         // 검증 로직 → 필드 단위 에러
@@ -147,6 +145,47 @@ public class ValidationItemControllerV2 {
             if (resultPrice < 10000) {
                 bindingResult.addError(new ObjectError("item", new String[]{"totalPriceMin"}, new Object[]{10000, resultPrice}, null));
                 // object error는 필드 에러가 아니라서 넘어올 값이 없다.
+            }
+        }
+
+        // 검증 실패시 다시 입력 폼으로
+        if (bindingResult.hasErrors()) { // errors에 값이 있다면, 즉 오류가 발생했다는 것을 의미한다..
+            log.info("error = {}", bindingResult);
+            return "validation/v2/addForm";
+        }
+
+        // 검증 성공 로직
+        Item savedItem = itemRepository.save(item);
+        redirectAttributes.addAttribute("itemId", savedItem.getId());
+        redirectAttributes.addAttribute("status", true);
+        return "redirect:/validation/v2/items/{itemId}";
+    }
+
+    @PostMapping("/add")
+    // BindingResult가 제공하는 rejectValue(), reject()를 사용하여 FieldError, ObjectError를 직접 생성하지 않고, 검증 오류를 다루기
+    public String addItemV4(@ModelAttribute Item item, BindingResult bindingResult, RedirectAttributes redirectAttributes, Model model) {
+
+        log.info("objectName = {}", bindingResult.getObjectName());
+        log.info("target = {}", bindingResult.getTarget());
+
+        // 검증 로직 → 필드 단위 에러
+        if (!StringUtils.hasText(item.getItemName())) {
+            bindingResult.rejectValue("itemName", "required");
+        }
+
+        if (item.getPrice() == null || item.getPrice() < 1000 || item.getPrice() > 1000000) {
+            bindingResult.rejectValue("price", "range", new Object[]{1000, 1000000}, null);
+        }
+
+        if (item.getQuantity() == null || item.getQuantity() >= 9999) {
+            bindingResult.rejectValue("quantity", "max", new Object[]{9999}, null);
+        }
+
+        // 특정 필드가 아닌 복합 룰 검증 → 필드 단위 에러 X
+        if (item.getPrice() != null && item.getQuantity() != null) {
+            int resultPrice = item.getPrice() * item.getQuantity();
+            if (resultPrice < 10000) {
+                bindingResult.reject("totalPriceMin", new Object[]{10000, resultPrice}, null);
             }
         }
 
