790427fcdc5276998356616dc9a418ef87ffac21
leehyeeun <leehe0430@gmail.com>
2022-06-08 18:22:03 +0900

    ch3 Q7 Two pointer with Sort

--- Algorithm/Algorithm_doit/src/main/java/Ch3_Basic/Q7TwoPointAndSortEx1.java
+++ Algorithm/Algorithm_doit/src/main/java/Ch3_Basic/Q7TwoPointAndSortEx1.java
@@ -7,43 +7,35 @@ import java.util.Arrays;
 import java.util.StringTokenizer;
 
 public class Q7TwoPointAndSortEx1 {
-
+    // 백준 1940
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        int N = Integer.parseInt(br.readLine()); // readLine()은 String 값을 반환
-        long[] arr = new long[N];
-        int cnt = 0;
+        int N = Integer.parseInt(br.readLine());
+        int M = Integer.parseInt(br.readLine());
+        int[] arr = new int[N];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
         for (int i = 0; i < N; i++) {
-            arr[i] = Long.parseLong(st.nextToken());
+            arr[i] = Integer.parseInt(st.nextToken());
         }
-
         Arrays.sort(arr);
 
-        for (int i = 0; i < N; i++) {
-            long find = arr[i];
-            int startIdx = 0; // 위치가 중요, 외부에 있으면 while에서 update한 startIdx, endIdx가 반영되지 않는다.
-            int endIdx = N - 1;
-            while (startIdx < endIdx) {
-                long sum = arr[startIdx] + arr[endIdx];
-                if (sum > find) {
-                    endIdx--;
-                } else if (sum < find) {
-                    startIdx++;
-                } else {
-                    if(startIdx != i && endIdx != i) {
-                        cnt++;
-                        break;
-                    } else if (startIdx == i) {
-                        startIdx++;
-                    } else if (endIdx == i) {
-                        endIdx--;
-                    }
-                }
+        int cnt = 0;
+        int start = 0;
+        int end = N - 1;
+        while (start < end) {
+            int sum = arr[start] + arr[end];
+            if (sum > M) {
+                end--;
+            } else if (sum < M) {
+                start++;
+            } else { // sum == M
+                cnt++;
+                start++;
+                end--;
             }
         }
         System.out.println(cnt);
-        br.close();
+
     }
 }
