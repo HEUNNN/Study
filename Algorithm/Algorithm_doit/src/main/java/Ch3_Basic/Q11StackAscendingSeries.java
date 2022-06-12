fe6f1214e08a685e7aae068c1ef195b73767eca5
leehyeeun <leehe0430@gmail.com>
2022-06-12 20:25:45 +0900

    ch3 Q12 Stack - 오름차순 출력

--- Algorithm/Algorithm_doit/src/main/java/Ch3_Basic/Q11StackAscendingSeries.java
+++ Algorithm/Algorithm_doit/src/main/java/Ch3_Basic/Q11StackAscendingSeries.java
@@ -1,46 +1,56 @@
 package Ch3_Basic;
 
+import java.io.BufferedWriter;
 import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.OptionalInt;
 import java.util.Scanner;
 import java.util.Stack;
 
-public class StackSeries {
+public class Q11StackAscendingSeries {
     // 백준 1874
     //  stack에 오름 차순으로 숫자를 저장할 떄(1, 2, 3, 4...) 어떻게 push, pop 해야 주어진 수열이 출력되는지 알아내는 문제이다.
     // 주어진 수열을 출력하지 못하는 케이스에는 NO를 출력한다.
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
-        int N = sc.nextInt(); // 주어질 수열의 원소 개수
+        //BufferedWriter sb = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringBuffer sb = new StringBuffer(); // BufferWriter 사용하면 시간초과됨
+        int N = sc.nextInt();
         int[] arr = new int[N];
-        int num = 1; // 자연수
-        boolean result = true;
-        Stack<Integer> stack = new Stack<>();
-        StringBuffer bf = new StringBuffer(); // +, -를 저장해둘 곳
 
         for (int i = 0; i < N; i++) {
             arr[i] = sc.nextInt();
         }
+
+        Stack<Integer> stack = new Stack<>();
+
+        int num = 1;
+        boolean result = true;
         for (int i = 0; i < N; i++) {
-            int curr = arr[i];
-            if (curr >= num) {
-                while (curr >= num) {
+            int target = arr[i];
+            if (num <= target) {
+                while (num <= target) {
                     stack.push(num++);
-                    bf.append("+\n");
+                    sb.append("+\n");
                 }
                 stack.pop();
-                bf.append("-\n");
-            } else {
+                sb.append("-\n");
+            } else { // target < num
                 int n = stack.pop();
-                if (n > curr) {
-                    System.out.println("NO");
+                if (n != target) {
                     result = false;
+                    System.out.println("NO");
                     break;
                 } else {
-                    bf.append("-\n");
+                    sb.append("-\n");
                 }
             }
         }
-        if (result) System.out.println(bf.toString());
+        if (result) {
+            System.out.println(sb.toString());
+        }
+
     }
 }
 
