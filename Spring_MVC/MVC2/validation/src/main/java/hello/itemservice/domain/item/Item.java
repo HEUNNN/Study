54c842d1cf143f39aed961280cc518cc83e967e7
leehyeeun <leehe0430@gmail.com>
2022-06-12 11:51:25 +0900

    Bean Validation 한계

--- Spring_MVC/MVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
+++ Spring_MVC/MVC2/validation/src/main/java/hello/itemservice/domain/item/Item.java
@@ -12,6 +12,7 @@ import javax.validation.constraints.NotNull;
 // @ScriptAssert(lang = "javascript", script = "_this.price * _this.quantity >= 10000", message = "가격의 총합이  10000원 넘게 입력해주세요.")
 public class Item {
 
+    // @NotNull // 수정 요구사항 추가 → 하지만 상품을 등록할 때는 id 필드 입력 칸이 없다. Bean Validation의 한계
     private Long id;
     @NotBlank(message = "공백 X")
     private String itemName;
