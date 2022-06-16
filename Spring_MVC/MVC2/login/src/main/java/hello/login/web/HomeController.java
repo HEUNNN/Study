551248eedf94a5b31fa5c90ff60a175f33cfcd2d
leehyeeun <leehe0430@gmail.com>
2022-06-16 16:58:15 +0900

    로그인/로그아웃 처리 - cookie 사용

--- Spring_MVC/MVC2/login/src/main/java/hello/login/web/HomeController.java
+++ Spring_MVC/MVC2/login/src/main/java/hello/login/web/HomeController.java
@@ -1,15 +1,38 @@
 package hello.login.web;
 
+import hello.login.domain.member.Member;
+import hello.login.domain.member.MemberRepository;
+import lombok.Data;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.CookieValue;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Slf4j
 @Controller
+@Data
 public class HomeController {
+    private final MemberRepository memberRepository;
 
-    @GetMapping("/")
+    // @GetMapping("/")
     public String home() {
         return "home";
+    }
+
+    // LoginController에서 login 처리에 성공하면 redirect:/, 즉 / 경로로 GET HTTP 요청이 날라온.
+    @GetMapping("/")
+    public String homeLogin(@CookieValue(name = "memberId", required = false) Long memberId, Model model) {
+        if (memberId == null) {
+            return "home";
+        }
+
+        // 로그인 성공한 사용자
+        Member loginMember = memberRepository.findById(memberId);
+        if (loginMember == null) {
+            return "home";
+        }
+        model.addAttribute("member", loginMember); // loginHome view에서 사용할 loginMember 데이터를 model에 담아서 넘겨준다.
+        return "loginHome";
     }
 }
--- Spring_MVC/MVC2/login/src/main/java/hello/login/web/login/LoginController.java
+++ Spring_MVC/MVC2/login/src/main/java/hello/login/web/login/LoginController.java
@@ -4,12 +4,15 @@ import hello.login.domain.login.LoginService;
 import hello.login.domain.member.Member;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.web.servlet.server.Session;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletResponse;
 import javax.validation.Valid;
 
 @Controller
@@ -24,7 +27,7 @@ public class LoginController {
     }
 
     @PostMapping("/login")
-    public String login(@Valid @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult) {
+    public String login(@Valid @ModelAttribute("loginForm") LoginForm loginForm, BindingResult bindingResult, HttpServletResponse response) {
         if (bindingResult.hasErrors()) { // 특정 필드의 문제가 아니다. 글로벌 오류이다.
             return "login/loginForm";
         }
@@ -40,7 +43,24 @@ public class LoginController {
 
         // 로그인 성공 처리
 
+        // 로그인에 성공하면 idCookie 를 응답에 넣어서 client에게 보낸다.
+        // 쿠키에 시간 정보를 주지 않으면 세션쿠키로 인식된다. 세션 쿠키는 브라우저 종료시 모두 종료된다.
+        // idCookie는 loginId값이 아닌 memberId 라는 이름으로 id 값을 넘긴다.
+        Cookie idCookie = new Cookie("memberId", String.valueOf(loginMember.getId())); // long 타입인 id 값을 String.valueOf()를 사용하여 String 타입으로 변환한다.
+        response.addCookie(idCookie);
 
         return "redirect:/";
+    }
+
+    @PostMapping("/logout") // loginHome.html에 보면 logout 버튼의 form의 method가 post 방식의 HTTP 요청을 보내온다.
+    public String logout(HttpServletResponse response) {
+        expireCookie(response, "memberId");
+        return "redirect:/";
+    }
+
+    private void expireCookie(HttpServletResponse response, String cookieName) {
+        Cookie cookie = new Cookie(cookieName, null);
+        cookie.setMaxAge(0); // 만료시킨 쿠키
+        response.addCookie(cookie);
     }
 }
