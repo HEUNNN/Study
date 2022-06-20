a9997ec9c0ec31daeef348d8985703c6b130772c
leehyeeun <leehe0430@gmail.com>
2022-06-20 22:50:53 +0900

    서블릿 예외 처리 - 오류 페이지 작동 원리

--- Spring_MVC/MVC2/exception/src/main/java/hello/exception/WebServerCustomizer.java
+++ Spring_MVC/MVC2/exception/src/main/java/hello/exception/WebServerCustomizer.java
@@ -16,6 +16,6 @@ public class WebServerCustomizer implements WebServerFactoryCustomizer<Configura
         ErrorPage errorPage500 = new ErrorPage(HttpStatus.INTERNAL_SERVER_ERROR, "/error-page/500");
         ErrorPage errorPageEx = new ErrorPage(RuntimeException.class, "/error-page/500");
 
-        factory.addErrorPages(errorPage404, errorPage500, errorPage500);
+        factory.addErrorPages(errorPage404, errorPage500, errorPageEx);
     }
 }
