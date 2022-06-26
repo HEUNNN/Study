2dc1c06028251c0f7e37cf86b04225ce4dd40f16
leehyeeun <leehe0430@gmail.com>
2022-06-26 23:44:39 +0900

    ch4 Q22 기수 정렬 ver1

--- Algorithm/Algorithm_doit/src/main/java/Ch4_Sort/Q22RadixSortVer1.java
+++ Algorithm/Algorithm_doit/src/main/java/Ch4_Sort/Q22RadixSortVer1.java
@@ -1,28 +1,28 @@
 package Ch4_Sort;
 
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
+import java.io.*;
 
 
-public class Q22RadixSort {
+public class Q22RadixSortVer1 {
     // 백준 10989
     private static int[] arr;
 
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
         int N = Integer.parseInt(br.readLine());
         arr = new int[N];
         for (int i = 0; i < N; i++) {
             arr[i] = Integer.parseInt(br.readLine());
         }
 
+        br.close();
         radixSort(arr, 5);
-
         for (int i = 0; i < N; i++) {
-            System.out.print(arr[i] + " ");
+            bw.write(arr[i] +"\n");
         }
-
+        bw.flush();
+        bw.close();
 
     }
 
@@ -37,6 +37,7 @@ public class Q22RadixSort {
                 int idx =( a[i] / jarisu) % 10;
                 bucket[idx]++;
             }
+
             for (int i = 1; i < bucket.length; i++) {
                 bucket[i] += bucket[i - 1]; // 예) 일의 자리수가 특정 숫자 n인 숫자들의 개수를 합 배열로 표현
             }
