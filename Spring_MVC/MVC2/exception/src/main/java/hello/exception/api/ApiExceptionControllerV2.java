5cfc8a9a86a5f532964d08dd79588e810c4b461c
leehyeeun <leehe0430@gmail.com>
2022-06-28 15:42:49 +0900

    ControllerAdvice

--- Spring_MVC/MVC2/exception/src/main/java/hello/exception/api/ApiExceptionControllerV2.java
+++ Spring_MVC/MVC2/exception/src/main/java/hello/exception/api/ApiExceptionControllerV2.java
@@ -1,10 +1,7 @@
 package hello.exception.api;
 
 import hello.exception.exception.UserException;
-import hello.exception.exhandler.ErrorResult;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import static hello.exception.api.ApiExceptionController.*;
@@ -13,35 +10,7 @@ import static hello.exception.api.ApiExceptionController.*;
 @RestController
 public class ApiExceptionControllerV2 {
 
-    @ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler
-//    @ExceptionHandler(IllegalArgumentException.class)
-    public ErrorResult illegalExHandle(IllegalArgumentException e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("BAD", e.getMessage());
-
-        /**
-         * {
-         *     "code": "BAD",
-         *     "message": "잘못된 입력 값"
-         * }
-         */
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<ErrorResult> userExHandle(UserException e) {
-        log.error("[exceptionHandle] ex", e);
-        ErrorResult errorResult = new ErrorResult("USER-EX", e.getMessage());
-        return new ResponseEntity<>(errorResult, HttpStatus.BAD_REQUEST);
-    }
-
-    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
-    @ExceptionHandler
-    public ErrorResult exHandle(Exception e) {
-        log.error("[exceptionHandle] ex", e);
-        return new ErrorResult("EX", "내부 오류");
-    }
-
+    // ControllerAdvice를 사용하여 예외를 처리하는 부분을 분리함
     @GetMapping("/api2/members/{id}")
     public MemberDto getMember(@PathVariable("id") String id) {
         if (id.equals("ex")) {
