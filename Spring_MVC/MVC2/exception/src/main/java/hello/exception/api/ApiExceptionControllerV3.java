30a9aea66501f9ff01f1c8073489b0179493acaa
leehyeeun <leehe0430@gmail.com>
2022-06-28 15:50:19 +0900

    대상 컨트롤러 지정 방법

---
+++ Spring_MVC/MVC2/exception/src/main/java/hello/exception/api/ApiExceptionControllerV3.java
@@ -0,0 +1,30 @@
+package hello.exception.api;
+
+import hello.exception.exception.UserException;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class ApiExceptionControllerV3 {
+    // ControllerAdvice를 사용하여 예외를 처리하는 부분을 분리함
+    @GetMapping("/api3/members/{id}")
+    public ApiExceptionController.MemberDto getMember(@PathVariable("id") String id) {
+        if (id.equals("ex")) {
+            throw new RuntimeException("잘못된 사용자");
+        }
+
+        if (id.equals("bad")) {
+            throw new IllegalArgumentException("잘못된 입력 값");
+        }
+
+        if (id.equals("user-ex")) {
+            throw new UserException("사용자 오류");
+        }
+
+        return new ApiExceptionController.MemberDto(id, "hello" + id);
+    }
+
+}
