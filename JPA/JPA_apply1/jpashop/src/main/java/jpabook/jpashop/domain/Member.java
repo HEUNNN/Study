38f90cf5d098c1867bada4b634505a11a41a283d
leehyeeun <leehe0430@gmail.com>
2022-06-30 17:08:31 +0900

    엔티티 클래스 개발 1

---
+++ JPA/JPA_apply1/jpashop/src/main/java/jpabook/jpashop/domain/Member.java
@@ -0,0 +1,27 @@
+package jpabook.jpashop.domain;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Getter
+@Setter
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member") // order table의 member 필드에 의해 매핑 되었다는 의미이다.
+    private List<Order> orders = new ArrayList<>();
+}
