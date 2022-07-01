65b84221b5c3abc60a3d69860c7c3ada6475b6a2
leehyeeun <leehe0430@gmail.com>
2022-07-01 13:39:35 +0900

    회원 리포지토리 개발

---
+++ JPA/JPA_apply1/jpashop/src/main/java/jpabook/jpashop/repository/MemberRepository.java
@@ -0,0 +1,36 @@
+package jpabook.jpashop.repository;
+
+import jpabook.jpashop.domain.Member;
+import org.springframework.stereotype.Repository;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+import java.util.List;
+
+@Repository
+public class MemberRepository {
+
+    @PersistenceContext
+    private EntityManager em; // spring이 entity 매니저를 만들어서 주입 해준다. JPA를 사용하기 때문에 필요하다.
+
+    public void save(Member member) {
+        em.persist(member);
+    }
+
+    public Member findOne(Long id) {
+        return em.find(Member.class, id);
+    }
+
+    public List<Member> findAll() {
+        List<Member> members = em.createQuery("select m from Member m", Member.class)
+                .getResultList();
+        return members;
+    }
+
+    public List<Member> findByName(String name) {
+        return em.createQuery("select m from Member m where m.name = :name", Member.class)
+                .setParameter("name", name)
+                .getResultList();
+    }
+
+}
