92da373374080ecbb026c2b994fd8d44aba4f8f7
leehyeeun <leehe0430@gmail.com>
2022-07-05 19:07:51 +0900

    영속성 컨텍스트 1

--- JPA/JPA_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/JPA_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -18,6 +18,19 @@ public class JpaMain {
         tx.begin(); // database transaction 시작
 
         try {
+
+            Member newMember = new Member();
+            newMember.setId(10L);
+            newMember.setName("apple");
+            // newMember는 비영속 상태
+
+            System.out.println("===Before===");
+            em.persist(newMember);
+            System.out.println("===After===");
+            // newMember는 영속 상태 → 엔티티 매니저에 들어 있는 영속성 컨텍스트라는 곳에서 newMember를 이제부터 관리한다는 뜻이다.
+            // 이때 DB에 저장되는 것이 아니다. DB에 저장되는지(쿼리가 날아가는지) 확인하기 위해 Before, After 출력
+
+
             // 이미 저장되어 있는 id: 1, name: HelloA 인 member 데이터를 수정하기
             Member findMember1 = em.find(Member.class, 1L);
 //            Member findMember2 = em.find(Member.class, 3L); // id가 1L인 데이터 찾기/**/
