13d7e44333ea0d4ed695bfaec35810d27bcf9252
leehyeeun <leehe0430@gmail.com>
2022-07-06 18:49:58 +0900

    양방향 연관관계와 연관관계의 주인 1 - 기본

--- JPA/JPA_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
+++ JPA/JPA_basic/ex1-hello-jpa/src/main/java/hellojpa/JpaMain.java
@@ -5,6 +5,7 @@ import javax.persistence.EntityManager;
 import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
 import javax.persistence.Persistence;
+import java.util.List;
 
 public class JpaMain {
 
@@ -21,10 +22,10 @@ public class JpaMain {
             Team team1 = new Team();
             team1.setName("TeamA");
             em.persist(team1); // persist하면 id 자동으로 생성되어 부여된다.
-
+/*
             Team team2 = new Team();
             team2.setName("TeamB");
-            em.persist(team2);
+            em.persist(team2);*/
 
             Member member1 = new Member();
             member1.setUsername("member1");
@@ -33,36 +34,28 @@ public class JpaMain {
             member1.setTeam(team1);
             em.persist(member1);
 
-            Member member2 = new Member();
-            member2.setUsername("member2");
-//            member2.setTeamId(team2.getId());
-            member2.setTeam(team2);
-            em.persist(member2);
+            em.flush();
+            em.clear();
 
-            Member member3 = new Member();
-            member3.setUsername("member3");
-//            member3.setTeamId(team1.getId());
-            member3.setTeam(team1);
-            em.persist(member3);
+            // 양방향 객체 연관관계를 생성하여 Team에서 Member 참조하기(양방향) → 그 전에 em.flush(), em.clear() 필요
+            Member findMember1 = em.find(Member.class, member1.getId());
+            List<Member> members = findMember1.getTeam().getMembers();
+            for (Member m : members) {
+                System.out.println("member = " + m.getUsername());
+            }
 
-            Member member4 = new Member();
-            member4.setUsername("member4");
-            member4.setTeam(team2);
-            em.persist(member4);
-
-            // 특정 멤버를 찾고, teamId 찾기
+/*            // 특정 멤버를 찾고, teamId 찾기
             Member findMember1 = em.find(Member.class, member2.getId()); // 영속성 컨텍스트에 들어가있는 상태이기 때문에 1차 캐시에서 가져온다. 쿼리를 사용하지 않는다.
             // 만약 영속성 컨텍스트 말고 DB에서 가져오게 하려면 em.persist(member3) 후에 em.flush()하여 DB에 쿼리를 날려 1차 캐시에 있는 데이터를 DB로 보내고, em.clear() 하여 영속성 컨텍스트를 초기화 한다.
-            Team findTeam1 = findMember1.getTeam();
-            System.out.println("findTeam = " + findTeam1.getName());
+            Team findTeam1 = findMember1.getTeam();*/
+ /*           System.out.println("findTeam = " + findTeam1.getName());
 
             findMember1.setTeam(team1);
 
             Member findMember2 = em.find(Member.class, member2.getId());
             Team findTeam2 = findMember2.getTeam();
             System.out.println("changed Team ");
-            System.out.println("findTeam = " + findTeam2.getName());
-
+            System.out.println("findTeam = " + findTeam2.getName());*/
 
             tx.commit();
         } catch (Exception e) {
--- JPA/JPA_basic/ex1-hello-jpa/src/main/java/hellojpa/Team.java
+++ JPA/JPA_basic/ex1-hello-jpa/src/main/java/hellojpa/Team.java
@@ -1,9 +1,8 @@
 package hellojpa;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.Id;
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 public class Team {
@@ -13,6 +12,18 @@ public class Team {
     @Column(name = "TEAM_ID")
     private Long id;
     private String name;
+
+    // Member : Team = N : 1, Team 입장에서는 @OneToMany
+    @OneToMany(mappedBy = "team")
+    private List<Member> members = new ArrayList<>();
+
+    public List<Member> getMembers() {
+        return members;
+    }
+
+    public void setMembers(List<Member> members) {
+        this.members = members;
+    }
 
     public Long getId() {
         return id;
