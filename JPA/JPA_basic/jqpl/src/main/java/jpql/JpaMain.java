99b77ffb1a9e8cab63929d3336af2b259f0c8f9f
leehyeeun <leehe0430@gmail.com>
2022-07-12 14:53:44 +0900

    JPQL 프로젝션

--- JPA/JPA_basic/jqpl/src/main/java/jpql/JpaMain.java
+++ JPA/JPA_basic/jqpl/src/main/java/jpql/JpaMain.java
@@ -1,6 +1,9 @@
 package jpql;
 
-import javax.persistence.*;
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.List;
 
 public class JpaMain {
@@ -19,7 +22,7 @@ public class JpaMain {
             member.setAge(10);
             em.persist(member);
 
-            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
+/*            TypedQuery<Member> query1 = em.createQuery("select m from Member m", Member.class);
             // TypeQuery: 반환 타입이 명확할 때 사용
             Query query2 = em.createQuery("select m.username, m.age from Member m");
             // Query: 반환 타입이 명확하지 않을 때 사용
@@ -31,9 +34,41 @@ public class JpaMain {
             // 파라미터 바인딩
             TypedQuery<Member> query3 = em.createQuery("select m from Member m where m.username = :username", Member.class);
             query3.setParameter("username", "member1");
-            Member singleResult3 = query3.getSingleResult();
+            Member singleResult3 = query3.getSingleResult();*/
+            em.flush();
+            em.clear();
 
-            System.out.println("single Result = " + singleResult3.getUsername());
+            List<Member> resultList1 = em.createQuery("select m from Member m", Member.class) // 엔티티 프로젝션(조회)이니까 조회되는 List<Member>의 Member도 엔티티이다.
+                    .getResultList();
+            // List<Member>도 영속성 컨텍스트에 의해 관리가 될까?
+            Member member1 = resultList1.get(0);
+            member1.setAge(20); // 변화가 있으면 영속성 컨텍스트에 의해 관리가 된다는 뜻이다.
+
+            // 임베디드 타입 프로젝션
+            List<Address> resultList2 = em.createQuery("select o.address from Order o", Address.class).getResultList();
+
+            // 스칼라 타입 프로젝션 + DISTINCT
+            List resultList3 = em.createQuery("select distinct m.username, m.age from Member m").getResultList();
+
+            // 쿼리 타입으로 여러 값 조회
+/*
+            Object o = resultList3.get(0);
+            Object[] result = (Object[]) o;
+            System.out.println("username = " + result[0]);
+            System.out.println("age = " + result[1]);
+*/
+
+            // Object[] 타입으로 여러 값 조회
+/*            List<Object[]> resultList4 = em.createQuery("select distinct m.username, m.age from Member m").getResultList();
+            Object[] objects = resultList4.get(0);
+            System.out.println("username = " + objects[0]);
+            System.out.println("age = " + objects[1]);*/
+
+            // new 명령어로 여러 값 조회
+            List<MemberDTO> resultList5 = em.createQuery("select new jpql.MemberDTO(m.username, m.age) from Member m", MemberDTO.class)
+                    .getResultList();
+            MemberDTO memberDTO = resultList5.get(0);
+            System.out.println("memberDTO = " + memberDTO.getUsername() + ", " + memberDTO.getAge());
 
             tx.commit();
         } catch (Exception e) {
