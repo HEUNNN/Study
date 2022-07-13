a96e12ce72537deeaee3f21ead306b365937d62d
leehyeeun <leehe0430@gmail.com>
2022-07-13 23:05:41 +0900

    프로그래머스 문자열 압축

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,7 +1,12 @@
-import java.util.Vector;
+import java.util.LinkedList;
+import java.util.Queue;
 
 public class Solution {
-    public static int solution(String s) {
+
+    public static void main(String[] args) {
+
+    }
+    public int solution(String s) {
         int n = s.length();
         int answer = n;
 
@@ -15,85 +20,42 @@ public class Solution {
         return answer;
     }
 
-    public static String pressedString(String s, int length) {
+    public String pressedString(String s, int length) {
 
-        Vector<String> v = new Vector();
+        Queue<String> q = new LinkedList<>();
         String tmp = ""; // 길이: 0
 
-        // vector에 length 만큼씩 잘라서 넣기
+        // queue에 length 만큼씩 잘라서 넣기
         for (int i = 0; i < s.length(); i++) {
             tmp += s.charAt(i);
 
             if (tmp.length() == length) {
-                v.add(tmp);
+                q.add(tmp);
                 tmp = "";
             }
         }
         if (tmp.length() >= 1) {
-            v.add(tmp);
+            q.add(tmp);
         }
 
         String res = "";
-        while (v.size() != 0) {
-
-            Integer cnt = 0;
-
-            for (int i = 1; i < v.size(); i++) {
-                if (v.get(0).equals(v.get(i))) {
+        while (!q.isEmpty()) {
+            int cnt = 0;
+            String cur = q.poll(); // 큐에서 한개 뽑기
+            while (!q.isEmpty()) {
+                if (cur.equals(q.peek())) {
                     cnt++;
+                    q.poll(); // cur 이랑 같으므로 cnt++ 후 큐에서 뽑기
                 } else {
                     break;
                 }
             }
-
             if (cnt == 0) {
-                res += v.get(0);
+                res += cur;
             } else {
-
-                res += ((cnt + 1) + v.get(0));
-            }
-
-            for (int i = 0; i <= cnt; i++) {
-                v.remove(0);
+                res += ((cnt + 1) + cur);
             }
         }
-
         return res;
     }
-
-    public int solution2(String s) {
-        int answer = 0;
-
-        if (s.length() == 1) {
-            return 1;
-        }
-        for (int i = 1; i <= (s.length() / 2); i++) {
-            int result = getSplitedLength(s, i, 1).length();
-            answer = i == 1 ? result : (Math.min(answer, result));
-        }
-
-        return answer;
-    }
-
-    public String getSplitedLength(String s, int n, int repeat) {
-        if (s.length() < n) return s;
-        String result = "";
-        String preString = s.substring(0, n);
-        String postString = s.substring(n, s.length());
-
-        // 불일치 -> 현재까지 [반복횟수 + 반복문자] 조합
-        if (!postString.startsWith(preString)) {
-            if (repeat == 1) return result += preString + getSplitedLength(postString, n, 1);
-            return result += Integer.toString(repeat) + preString + getSplitedLength(postString, n, 1);
-        }
-
-        return result += getSplitedLength(postString, n, repeat + 1);
-    }
-
 }
-
-
-
-
-
-
