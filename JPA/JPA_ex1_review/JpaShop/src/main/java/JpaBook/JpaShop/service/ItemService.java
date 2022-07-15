a4a4c5875354e3acd0cef79de20bb55f05426914
leehyeeun <leehe0430@gmail.com>
2022-07-15 16:49:09 +0900

    ItemService

---
+++ JPA/JPA_ex1_review/JpaShop/src/main/java/JpaBook/JpaShop/service/ItemService.java
@@ -0,0 +1,32 @@
+package JpaBook.JpaShop.service;
+
+import JpaBook.JpaShop.domain.item.Item;
+import JpaBook.JpaShop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly = true)
+@RequiredArgsConstructor
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void savItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    public List<Item> findItems() {
+        List<Item> items = itemRepository.findAll();
+        return items;
+    }
+
+    public Item findOne(Long id) {
+        Item findItem = itemRepository.findOne(id);
+        return findItem;
+    }
+}
