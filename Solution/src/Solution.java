f736ea14fcc99e468b9ecfb5d0c7c31582d514a8
leehyeeun <leehe0430@gmail.com>
2022-07-17 11:46:26 +0900

    프로그래머스 신규아이디 추천

--- Solution/src/Solution.java
+++ Solution/src/Solution.java
@@ -1,34 +1,95 @@
-public class Solution {
+import java.util.Deque;
+import java.util.LinkedList;
 
+public class Solution {
     public static void main(String[] args) {
-        long gcd = gcd(5, 3);
-        System.out.println(gcd);
+        String new_id1 = "...!@BaT#*..y.abcdefghijklm";
+        String new_id2 = "z-+.^.";
+        String new_id3 = "=.=";
+        String new_id4 = "123_.def";
+        String new_id5 = "abcdefghijklmn.p";
+
+        String answer = solution(new_id5);
+        System.out.println(answer);
     }
 
-    public static long solution(long w, long h) {
+    public static String solution(String new_id) {
 
-        long rectCnt = 0;
+        String answer = "";
 
+        // 1단계
+        String lowerCase = new_id.toLowerCase();
 
-        for (long i = 1; i < w; i++) {
-            double v = (double) h * i;
-            rectCnt += (long) (v / w);
+        // 2단계
+        String check = "~!@#$%^&*()=+[{]}:?,<>/";
+
+        Deque<Character> dq1 = new LinkedList<>();
+        Deque<Character> dq2 = new LinkedList<>();
+
+        for (int i = 0; i < lowerCase.length(); i++) {
+            if (check.indexOf(lowerCase.charAt(i)) == -1) {
+                dq1.add(lowerCase.charAt(i));
+            }
         }
 
-        return rectCnt * 2;
+        // 3단계
+        while (!dq1.isEmpty()) { // poll == pollFirst
+            Character tmp = dq1.pollFirst();
+            if (tmp == '.') {
+                while (dq1.size() >= 1 && dq1.peekFirst() == '.') {
+                    dq1.pollFirst();
+                }
+                dq2.addLast(tmp);
+            } else {
+                dq2.addLast(tmp);
+            }
+        }
+
+        dq1 = copyDQ(dq2);
+
+        // 4단계
+
+        if (dq1.size() >= 1 && dq1.peekFirst() == '.') {
+            dq1.pollFirst();
+        } else if (dq1.size() >= 1  && dq1.peekLast() == '.') {
+            dq1.pollLast();
+        }
+
+        // 5단계
+        if (dq1.isEmpty()) {
+            dq1.add('a');
+        }
+
+        // 6단계
+        if (dq1.size() >= 16) {
+            while (dq1.size() >= 16) {
+                dq1.pollLast();
+            }
+        }
+
+        while (dq1.peekLast() == '.') {
+            dq1.pollLast();
+        }
+
+        // 7단계
+        if (dq1.size() <= 2) {
+            while (dq1.size() < 3) {
+                dq1.add(dq1.peekLast());
+            }
+        }
+
+        while (!dq1.isEmpty()) {
+            answer += dq1.poll();
+        }
+
+        return answer;
     }
-    public static long solution2(long w, long h) {
-        long result = 0L;
-        long gcd = gcd(w, h);
-        result = (w * h) - ((w + h) - gcd);
-        return result;
-    }
+    public static Deque<Character> copyDQ(Deque<Character> dq) {
+        Deque<Character> newDq = new LinkedList<>();
+        while (!dq.isEmpty()) {
+            newDq.addLast(dq.pollFirst());
+        }
 
-
-    public static long gcd(long x, long y) {
-
-        // x < y 여도 gcd(y, x % y)에 의해 큰 수가 앞으로 오도록 자리를 잡게된다.
-        if (y == 0) return x;
-        else return gcd(y, x % y);
+        return newDq;
     }
 }
