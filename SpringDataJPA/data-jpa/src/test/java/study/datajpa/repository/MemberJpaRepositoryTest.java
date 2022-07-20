77d71516ad3f2eb3570bb4896fd45cf58983d1b8
leehyeeun <leehe0430@gmail.com>
2022-07-20 13:02:26 +0900

    순수 JPA 기반 리포지토리 만들기

--- SpringDataJPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
+++ SpringDataJPA/data-jpa/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -7,6 +7,8 @@ import org.springframework.test.annotation.Rollback;
 import org.springframework.transaction.annotation.Transactional;
 import study.datajpa.domain.Member;
 
+import java.util.List;
+
 import static org.assertj.core.api.Assertions.assertThat;
 
 
@@ -32,6 +34,41 @@ class MemberJpaRepositoryTest {
         assertThat(findMember).isEqualTo(member);
         // JPA는 엔티티의 동일성을 보장해준다(1차 캐시). 하지만 member를 save 한 후 flush, clear 하고 find 멤버를 한 경우에는 동일성 보장 X
 
+
+    }
+
+    @Test
+    public void basicCRUD() {
+        Member member1 = new Member("Lee hye");
+        Member member2 = new Member("Lee eun");
+
+        memberJpaRepository.save(member1);
+        memberJpaRepository.save(member2);
+
+        // 단건 조회 검증
+        Member findMember1 = memberJpaRepository.findById(member1.getId()).get();
+        Member findMember2 = memberJpaRepository.findById(member2.getId()).get();
+        assertThat(findMember1).isEqualTo(member1);
+        assertThat(findMember2).isEqualTo(member2);
+
+        // 리스트 조회 검증
+        List<Member> members = memberJpaRepository.findAll();
+        assertThat(members.size()).isEqualTo(2);
+
+        // JPA의 DirtyCheck
+        member1.setUsername("kim");
+        System.out.println("DirtyCheck: " + member1.getUsername());
+
+        // 카운트 검증
+        long count = memberJpaRepository.count();
+        assertThat(count).isEqualTo(2);
+
+        // 삭제 검증
+        memberJpaRepository.delete(member1);
+        memberJpaRepository.delete(member2);
+
+        long deletedCount = memberJpaRepository.count();
+        assertThat(deletedCount).isEqualTo(0);
 
     }
 }
