fd8ecd9b871009004ef005edbb6500a79ed90e53
leehyeeun <leehe0430@gmail.com>
2022-07-20 13:22:39 +0900

    공통 인터페이스 적용

--- SpringDataJPA/data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
+++ SpringDataJPA/data-jpa/src/main/java/study/datajpa/repository/TeamRepository.java
@@ -1,41 +1,7 @@
 package study.datajpa.repository;
 
-import org.springframework.stereotype.Repository;
+import org.springframework.data.jpa.repository.JpaRepository;
 import study.datajpa.domain.Team;
 
-import javax.persistence.EntityManager;
-import javax.persistence.PersistenceContext;
-import java.util.List;
-import java.util.Optional;
-
-@Repository
-public class TeamRepository {
-
-    @PersistenceContext
-    EntityManager em;
-
-    public Team save(Team team) {
-        em.persist(team);
-        return team;
-    }
-
-    public void delete(Team team) {
-        em.remove(team);
-    }
-
-    public List<Team> findAll() {
-        List<Team> teams = em.createQuery("select t from Team t", Team.class).getResultList();
-        return teams;
-    }
-
-    public Optional<Team> findById(Long id) {
-        Team team = em.find(Team.class, id);
-        Optional<Team> optionalTeam = Optional.ofNullable(team);
-        return optionalTeam;
-    }
-
-    public long count() {
-        Long countRes = em.createQuery("select count(t) from Team t", Long.class).getSingleResult();
-        return countRes;
-    }
+public interface TeamRepository extends JpaRepository<Team, Long> {
 }
