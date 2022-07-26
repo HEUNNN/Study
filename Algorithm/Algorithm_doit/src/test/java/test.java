2746a3041d9c95260832e08f5b470b2976278926
leehyeeun <leehe0430@gmail.com>
2022-07-26 13:15:08 +0900

    ch8 Graph - 이분 그래프

---
+++ Algorithm/Algorithm_doit/src/test/java/test.java
@@ -0,0 +1,10 @@
+public class test {
+    public static void main(String[] args) {
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println(arr[i]);
+        }
+
+    }
+}
