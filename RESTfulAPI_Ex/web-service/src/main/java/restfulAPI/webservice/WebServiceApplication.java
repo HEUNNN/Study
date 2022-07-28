098b86670dc7bb5c6c835180b35938ad1ef38d3f
leehyeeun <leehe0430@gmail.com>
2022-07-28 16:46:50 +0900

    encoding 설정 바꾸기 전

--- RESTfulAPI_Ex/web-service/src/main/java/restfulAPI/webservice/WebServiceApplication.java
+++ RESTfulAPI_Ex/web-service/src/main/java/restfulAPI/webservice/WebServiceApplication.java
@@ -2,11 +2,23 @@ package restfulAPI.webservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.LocaleResolver;
+import org.springframework.web.servlet.i18n.SessionLocaleResolver;
+
+import java.util.Locale;
 
 @SpringBootApplication
 public class WebServiceApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WebServiceApplication.class, args);
+	}
+
+	@Bean
+	public LocaleResolver localResolver() {
+		SessionLocaleResolver localeResolver = new SessionLocaleResolver(); // 세션을 통해서 Locale 값을 얻어올 예정이다.
+		localeResolver.setDefaultLocale(Locale.KOREA);
+		return localeResolver;
 	}
 }
