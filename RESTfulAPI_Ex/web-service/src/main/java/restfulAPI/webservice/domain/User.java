c77ad522e315c5ef50a514dd7f55b18fda3f7a17
leehyeeun <leehe0430@gmail.com>
2022-07-28 17:47:54 +0900

    Response 데이터 제어 - @JsonIgnore, @JsonIgnoreProperties

--- RESTfulAPI_Ex/web-service/src/main/java/restfulAPI/webservice/domain/User.java
+++ RESTfulAPI_Ex/web-service/src/main/java/restfulAPI/webservice/domain/User.java
@@ -1,5 +1,7 @@
 package restfulAPI.webservice.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,6 +16,7 @@ import java.util.Date;
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
+@JsonIgnoreProperties(value = {"password", "ssn"})
 public class User {
 
     @NotNull
@@ -25,4 +28,11 @@ public class User {
 
     @Past // 과거 데이터만 사용할 수 있다는 validation 조건 걸어준다.
     private Date joinDate;
+
+    // 중요한 필드라서 cliend가 원본을 확인할 수 없도록 해야한다.
+//    @JsonIgnore // 해당 데이터 값을 숨겨서 응답으로 client에게 보여준다.(필드 명도 숨긴다.) + class level에도 붙일 수 있다.
+    private String password;
+
+//    @JsonIgnore
+    private String ssn;
 }
