fb37a00a8efdc33ae6d373d63be6973c7e7d9dc8
leehyeeun <leehe0430@gmail.com>
2022-08-02 21:45:36 +0900

    Configuration class - 사용자 인증 처리

---
+++ RESTfulAPI_Ex/web-service/src/main/java/restfulAPI/webservice/config/SecurityConfig.java
@@ -0,0 +1,34 @@
+package restfulAPI.webservice.config;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+import static org.springframework.security.config.Customizer.withDefaults;
+
+@Configuration
+public class SecurityConfig {
+    @Bean
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        http
+                .authorizeHttpRequests((authz) -> authz
+                        .anyRequest().authenticated()
+                )
+                .httpBasic(withDefaults());
+        return http.build();
+    }
+
+    @Autowired
+    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("kenneth")
+                .password("{noop}test1234")
+                .roles("USER"); // Login이 완료되면 USER 권한을 갖도록 한다.
+        // {noop} → 인코딩 없이 사용하겠다는 의미
+    }
+
+
+}
