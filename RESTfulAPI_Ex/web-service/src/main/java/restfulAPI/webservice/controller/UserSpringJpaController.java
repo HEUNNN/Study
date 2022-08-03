c83d681e7d6ed556a5fb3a2a02a6ae615196ac7e
leehyeeun <leehe0430@gmail.com>
2022-08-03 17:55:25 +0900

    Spring data JPA를 이용한 사용자 목록 조회 - GET HTTP Method

--- RESTfulAPI_Ex/web-service/src/main/java/restfulAPI/webservice/controller/UserSpringJpaController.java
+++ RESTfulAPI_Ex/web-service/src/main/java/restfulAPI/webservice/controller/UserSpringJpaController.java
@@ -4,16 +4,24 @@ import com.fasterxml.jackson.databind.ser.FilterProvider;
 import com.fasterxml.jackson.databind.ser.impl.SimpleBeanPropertyFilter;
 import com.fasterxml.jackson.databind.ser.impl.SimpleFilterProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
+import org.springframework.hateoas.server.mvc.WebMvcLinkBuilder;
 import org.springframework.http.converter.json.MappingJacksonValue;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import restfulAPI.webservice.domain.User;
+import restfulAPI.webservice.exception.UserNotFoundException;
 import restfulAPI.webservice.repository.UserRepository;
 
 import javax.annotation.PostConstruct;
 import java.util.Date;
 import java.util.List;
+import java.util.Optional;
+
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.linkTo;
+import static org.springframework.hateoas.server.mvc.WebMvcLinkBuilder.methodOn;
 
 @RestController
 @RequestMapping("/jpa")
@@ -27,6 +35,20 @@ public class UserSpringJpaController {
         List<User> users = userRepository.findAll();
         return users;
     }
+
+    @GetMapping("/users/{id}")
+    public EntityModel<User> retrieveUser(@PathVariable int id) {
+        Optional<User> userOpt = userRepository.findById(id);
+        if (userOpt.isEmpty()) {
+            throw new UserNotFoundException(String.format("ID[%s] not found", id));
+        }
+        // HATEOAS
+        EntityModel<User> model = EntityModel.of(userOpt.get());
+        WebMvcLinkBuilder linkTo = linkTo(methodOn(this.getClass()).retrieveAllUsers()); // 위에 생성한 retrieveAllUsers 메서드
+        model.add(linkTo.withRel("all-users"));
+        return model;
+    }
+
     @PostConstruct
     public void init() {
         userRepository.save(new User(1, "user1", new Date(), "pass1", "111111"));
